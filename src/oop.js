2e41f503d4a142d51f1b5443f1ea2d026f84f6f7
Andrey Minnikhanov <61944225+razdolbaipozhizni@users.noreply.github.com>
2023-12-03 01:09:14 +0500

    Update oop.js

--- src/oop.js
+++ src/oop.js
@@ -3,7 +3,17 @@
  * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
  * Реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
  */
-class Point {}
+class Point {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+      }
+    
+      distanceToOrigin() {
+        // Расстояние от точки до центра координат (0, 0) по формуле sqrt(x^2 + y^2)
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+      }
+}
 
 /**
  * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
@@ -11,7 +21,18 @@ class Point {}
  * Реализовать статический метод, который возвращает расстояние между Point3D.
  */
 class Point3D extends Point {
-    static vectorLength(a, b) {}
+    constructor(x = 0, y = 0, z = 0) {
+        super(x, y);
+        this.z = z;
+      }
+    
+      static vectorLength(pointA, pointB) {
+        // Расстояние между двумя точками в трехмерном пространстве по формуле sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+        const dx = pointB.x - pointA.x;
+        const dy = pointB.y - pointA.y;
+        const dz = pointB.z - pointA.z;
+        return Math.sqrt(dx ** 2 + dy ** 2 + dz ** 2);
+      }
 }
 
 /**
@@ -20,10 +41,25 @@ class Point3D extends Point {
  * Для тех, кто доверяет, но проверяет: написать тесты на методы класса (oop.spec.js)
  */
 class Queue {
-    push;
-    pop;
-    size;
-    clear;
+    constructor(initialArray = []) {
+        this.queue = [...initialArray];
+      }
+    
+      push(...elements) {
+        this.queue.push(...elements);
+      }
+    
+      pop() {
+        return this.queue.shift();
+      }
+    
+      get size() {
+        return this.queue.length;
+      }
+    
+      clear() {
+        this.queue = [];
+      }
 }
 
 module.exports = {
